b0707b55b3c1c98bff6e605296cdeeb0ba30380f
Gerard Paapu <sharkbrainguy@gmail.com>
2010-11-18 16:56:26 +1300

    added asynchronous loading and documentation to Embedly

--- Source/Chorus.Embedly.js
+++ Source/Chorus.Embedly.js
@@ -1,30 +1,148 @@
-(function (){
+// Embedly
+// ======
+//
+// A client for the [Embedly](http://embed.ly) service. 
+//
+// Embedly provides embedded media (images, movies, etc.) for links to e.g.
+// twitpic or youtube. Embedly supports over 100 services.
+//
+// To use the service, just call `Embedly.make(link)` where link is an anchor
+// element or url. `make` returns a placeholder element that you must place
+// where you would like the media to load. When the media does load, it will 
+// replace the placeholder.
+//
+// (Placeholders have the class `.embedly_placeholder`, it's best to style them
+// to be inconspicuous.)
+//
+//  A simple example:
+//
+//     $$("a.replaceme").each(function (el) {
+//         Embedly.make(el).inject(el, "after");
+//     });
+//
+//  This will insert embedded media after each of the links with the class
+//  "replaceme" that are supported by Embedly.
+
 var Embedly = {
-    'make': function (link){
-        var url = link.get('href');
-        var placeholder = new Element('div', {'class': "embed"}); 
+    // each supported service in the form 
+    // services[name] = [regex, ...]
+    // e.g. services['youtube'] = ["http:\\/\\/.*youtube\\.com\\/watch.*", ...]
+    services: {}, 
+
+    // A flat list of regexes for all supported services
+    patterns: [],
+
+    // Before `servicesLoaded` all calls to `make` are issued a placeholder
+    // and their request is pushed onto the queue. When the services are
+    // loaded, the queue is processed and each request fulfilled.
+    queue: [],
+
+    servicesLoaded: false,
+
+    // has `loadServices` been called?
+    initialized: false,
+
+    // Make the first request to the Embedly API to get the list of 
+    // supported services, when they come back populate `services`,
+    // `patterns` and fire `onServicesLoaded` 
+    loadServices: function () {
+        var embedly = this;
+        initialized = true;
+
+        new Request.JSONP({
+            url: "http://api.embed.ly/1/services/javascript",
+            onComplete: function (json) {
+                json.each(function (item, index){
+                    embedly.services[item.name] = item.regex;
+                    [].push.apply(embedly.patterns, item);
+                });      
+
+                embedly.onServicesLoaded();
+            }
+        }).send();
+    },
+
+    onServicesLoaded: function () {
+        this.servicesLoaded = true;
+        this.processQueue();
+    },
+
+    processQueue: function () {
+        this.queue.each(function (item) {
+            var url = item.url,
+                element = item.element;
+
+            if (this.supported(url)) {
+                this.__make__(url, placeholder);    
+            }
+        });
+    },
+
+    // The main interface for users, always returns a placeholder element
+    // the element may not ever be replaced, so it should be styled
+    // to be inconspicuous e.g. height: 0; visibility: hidden;
+    make: function (link){
+        var url = link instanceof Element ? link.get('href') : link,
+            placeholder = new Element('div', {'class': "embed embedly_placeholder"}); 
+
+        if (!this.initialized) {
+            this.loadServices();
+        }
+
+        if (!this.servicesLoaded) {
+            this.queue.push({url: url, element: placeholder});
+        } else if (this.supported(url)) {
+            this.__make__(url, placeholder); 
+        }
+
+        return placeholder;
+    },
+
+    // __make__ should only be called when the services are loaded
+    // and the url matches one of the supported patterns. It makes
+    // the call to the embedly api and replaces the placeholder
+    // with the final embedded media
+    __make__: function (url, placeholder) {
         new Request.JSONP({
             'url': "http://api.embed.ly/v1/api/oembed",
             'data': { 'url': url },
             'onComplete': function (json){
                 var el = Embedly.fromJSON(json);
-                if (el) el.replaces(placeholder);
+                if (el) {
+                    el.replaces(placeholder);
+                }
             }
         }).send();
-
-        return placeholder;
     },
-    
-    'fromJSON': function (json){
+
+    // Should only be called after the services have loaded
+    // returns true if a url matches any of the supported
+    // regexes
+    supported: function (url) {
+        if (!this.servicesLoaded) {
+            throw Error("Services not loaded");
+        }
+
+        return this.patterns.some(function (pattern) {
+            return pattern.test(url);
+        });
+    },
+
+    // Processes the JSON that is returned from embedly
+    // and returns an Element or false if the response is
+    // bad. The types are documented here:
+    //
+    // http://api.embed.ly/docs/oembed#oembed-types
+    fromJSON: function (json){
         var type = json.type;
-        return json.thumbnail_url ? Embedly.thumbnail(json) 
-            :  type === 'photo'   ? Embedly.photo(json)
-            :  type === 'video' 
-            || type === 'rich'    ? Embedly.html(json)
+        return json.thumbnail_url ? Embedly.toThumbnail(json) 
+            :  type === 'photo'   ? Embedly.toPhoto(json)
+            :  type === 'video' || 
+               type === 'rich'    ? Embedly.toHtml(json)
             :  false;
     },
 
-    'photo': function (json){
+    toPhoto: function (json){
         return new Element("img", {
             'class': "embed",
             'src': json.url,
@@ -33,7 +151,7 @@ var Embedly = {
         });
     },
 
-    'thumbnail': function (json){
+    toThumbnail: function (json){
         var link = new Element("a", {'href': json.url, 'class': "embed"});
         var img = new Element("img", {
             'src': json.thumbnail_url,
@@ -44,33 +162,9 @@ var Embedly = {
         return link.grab(img);
     },
 
-    'html': function (json){
+    toHtml: function (json){
         var el = new Element("div", {'html': json.html, 'class': "embed"}); 
         el.setStyles({'width': json.width, 'height': json.height });
         return el;
-    },
-
-    'renderExtras': function (name){
-        var patterns = $A(arguments).map(function (name){
-            var service = Embedly.services[name];
-            return (service || []).map(function (str){
-                return new RegExp(str, 'i');
-            });
-        }).flatten();
-
-        function test(link){
-            var url = link.get('href');
-            return patterns.some(function (pattern){
-                return url.match(pattern);
-            });
-        }
-
-        return function (_, body){
-            var links = body.getElements('a').filter(test);
-            return links.map(Embedly.make);
-        }
     }
-}
-
-Chorus.Embedly = Embedly;
-}());
+};
